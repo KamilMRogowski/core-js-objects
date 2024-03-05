6bcbca58f6d217ac08fdf64d654735471778051e
KamilMRogowski <98821210+KamilMRogowski@users.noreply.github.com>
2024-03-05 14:48:11 +0100

    feat: finish sellTickets task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -157,8 +157,17 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const ticketPrice = 25;
+  let sellerChange = 0;
+  for (let i = 0; i <= queue.length; i += 1) {
+    const customerChange = queue[i] - ticketPrice;
+    sellerChange += ticketPrice - customerChange;
+    if (sellerChange < 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
